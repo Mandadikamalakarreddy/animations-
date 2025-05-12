affa2d704eb6e9d94c28d23af7f1d0fabb1d18e9
kamalakar <reddykamalakar29@gmail.com>
2025-05-12 16:40:16 +0530

    updated the public folder

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+     <h1 className="font-circular text-3xl">This uses Circular Web font</h1>
+<p className="font-general">This uses General font</p>
+<h2 className="font-robert-medium">This uses Robert Medium font</h2>
+<p className="font-robert-regular">This uses Robert Regular font</p>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
+import HomePage from "./components/Home";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Welcome to My Next.js App!</h1>
-      <p className="text-lg mb-8">This is a simple example of a Next.js application.</p>
-
-      <p className="mt-4 text-gray-600">
-        This is a sample image of the Next.js logo. You can replace it with any image you like.
-      </p>
-    </div>
+<main>
+  <HomePage/>
+</main>
   );
 }
